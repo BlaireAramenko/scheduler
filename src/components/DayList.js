88e270ea1d89450c2c7068d96f6dcb3c631beee3
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-03-22 16:53:46 -0700

    incorporate the DayList component into the <Application>

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,8 +1,23 @@
 import React from "react";
-import "components/DayListItem";
+import DayListItem from "./DayListItem";
 
-export default function DayList() {
-  return (
-    <ul></ul>
-  );
+export default function DayList(props) {
+  //map over days array from props to create an array of DayListItem components
+  const days = props.days.map(day => (
+    //create a new DayListItem component for each day
+    <DayListItem
+    //set the unique key attribute to the day's id
+    key={day.id}
+    //set name attribute to the day's name
+    name={day.name}
+    //set the spots attrib to the day's number of spots
+    spots={day.spots}
+    //set the selected attrib to true if the day's name matches the selected day in props
+    selected={day.name === props.day}
+    //set the setDay attrib to the setDay function passed in through props
+    setDay={props.setDay}
+    />
+  ));
+  //render an unordered list with the array of DayListItem components inside it
+  return <ul>{days}</ul>;
 }
