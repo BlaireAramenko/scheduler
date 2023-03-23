0824ca722aba029a6b1ba927149fa2617f624034
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-03-23 14:32:21 -0700

    add interviewer list and make images clickable

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,54 @@
+import React from "react";
+import InterviewerListItem from "components/InterviewerListItem";
+import classNames from "classnames";
+import "components/InterviewerList.scss";
+
+/*export default function InterviewerList(props) {
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list"></ul>
+    </section>
+  );
+}*/
+
+/*export default function InterviewerList(props) {
+  //create a new array of InterviewerListItem components by using the map function to iterate over each interviewer in the props.interviewers array
+  const interviewers = props.interviewers.map(interviewer => (
+    <InterviewerListItem
+    key={interviewer.id}
+    name={interviewer.name}
+    avatar={interviewer.avatar}
+    selected={interviewer.id === props.interviewer}
+    setInterviewer={interviewer.setInterviewer}
+    />
+  ));
+  return (
+    <section className="interviewers" >
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  );
+} */
+
+export default function InterviewerList(props) {
+  //create a new array of InterviewerListItem components by using the map function to iterate over each interviewer in the props.interviewers array
+  const interviewers = props.interviewers.map(interviewer => (
+    <InterviewerListItem
+    key={interviewer.id}
+    id={interviewer.id}
+    name={interviewer.name}
+    avatar={interviewer.avatar}
+    selected={interviewer.id === props.interviewer}
+    setInterviewer={props.setInterviewer}
+
+    />
+  ));
+  return (
+    <section className="interviewers" >
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  );
+}
+  
