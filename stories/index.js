f8b4cd2e400aec6e2bbcdd71da5c40d9a8a8ec51
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-03-24 12:48:14 -0700

    add props and state to appointment form

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -169,5 +170,21 @@ storiesOf("InterviewerListItem", module)
     <Error
     message="Could not delete appointment."
     onClose={action("onClose")}
+    />
+  ))
+  .add("Edit form", () => (
+    <Form 
+      name="Blaire"   
+      interviewer={2}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Create form", () => (
+    <Form 
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ))
