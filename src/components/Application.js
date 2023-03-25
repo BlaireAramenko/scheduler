01a8e01d4e788588294925ffbb03ada3310c9664
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-03-25 16:19:26 -0700

    retrieve interviewer data

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import DayList from "./DayList";
 import React, { useState, useEffect } from "react";
 import Appointment from "./Appointment";
 import axios from "axios";
-import { getAppointmentsForDay } from "../helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
 
 
 export default function Application(props) {
@@ -11,18 +11,21 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"),
-      axios.get("/api/appointments")
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ])
       .then((all) => {
-        console.log(all[0].data); //days data
-        console.log(all[1].data); //appointments data
-        setState(prevState => ({ ...prevState, days: all[0].data, appointments: all[1].data }));
+       //console.log(all[0].data); //days data
+        //console.log(all[1].data); //appointments data
+        //console.log(all[2].data); //interviewers data
+        setState(prevState => ({ ...prevState, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
      })
       //if there's an error, log it to the console
       .catch((error) => {
@@ -30,7 +33,20 @@ export default function Application(props) {
       });
   }, []); //run this hook only once after the component mounts 
 
-  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const appointments = getAppointmentsForDay(state, state.day);
+
+  const schedule = appointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
+
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
+  });
 
 
   return (
@@ -60,13 +76,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {dailyAppointments.map(appointment => (
-          <Appointment
-            key={appointment.id}
-            {...appointment}
-            time="5pm"
-          />
-        ))}
+        {schedule}
       </section>
     </main>
   );
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -8,3 +8,22 @@ export function getAppointmentsForDay(state, day) {
   //return the array of appointment objects 
   return appointments;
 }
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  const interviewerId = interview.interviewer;
+  const interviewer = state.interviewers[interviewerId];
+  if (!interviewer) {
+    return null;
+  }
+  return {
+    student: interview.student,
+    interviewer: {
+      id: interviewer.id,
+      name: interviewer.name,
+      avatar: interviewer.avatar
+    }
+  };
+}
