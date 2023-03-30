15c06dce97b61a20f829e632bfc6fe3fdc18f17e
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-03-29 17:00:40 -0700

    refactor form test

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -53,8 +53,8 @@ export default function Form(props) {
             onChange={event => setStudent(event.target.value)}
             data-testid="student-name-input"
           />
-          <section className="appointment__validation">{error}</section>
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
