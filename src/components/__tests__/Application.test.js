8c94248626aee14fd7540f8660f1c6e6a8093b6a
BlaireAramenko <blaire.aramenko21@gmail.com>
2023-03-29 20:54:45 -0700

    add test for editing an interview

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { render, cleanup, waitForElement, fireEvent, prettyDOM, getByText, getAllByTestId, getByPlaceholderText, getByAltText, queryByText, queryByAltText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, prettyDOM, getByText, getAllByTestId, getByPlaceholderText, getByAltText, queryByText, queryByAltText, waitForElementToBeRemoved } from "@testing-library/react";
 import Application from "components/Application";
 import axios from "axios";
 
 afterEach(cleanup);
 
 describe("Application", () => {
+
+  //CHANGES THE SCHEDULE
 it("changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
 
@@ -16,6 +18,7 @@ it("changes the schedule when a new day is selected", async () => {
   expect(getByText("Leopold Silvers")).toBeInTheDocument();
 });
 
+//BOOKS INTERVIEW
 it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
   const { container } = render(<Application />);
 
@@ -44,6 +47,7 @@ it("loads data, books an interview and reduces the spots remaining for Monday by
   expect(getByText(day, "no spots remaining")).toBeInTheDocument();
 });
 
+//CANCELS INTERVIEW
 it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
   // 1. Render the Application.
   const { container } = render(<Application />);
@@ -59,8 +63,7 @@ it("loads data, cancels an interview and increases the spots remaining for Monda
   fireEvent.click(queryByAltText(appointment, "Delete"));
 
   // 4. Check that the confirmation message is shown.
-  expect(
-    getByText(appointment, "Are you sure you would like to delete the appointment?")
+  expect(getByText(appointment, "Are you sure you would like to delete the appointment?")
   ).toBeInTheDocument();
 
   // 5. Click the "Confirm" button on the confirmation.
@@ -80,9 +83,29 @@ it("loads data, cancels an interview and increases the spots remaining for Monda
   expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
 });
 
-/* test number five */
-it("shows the save error when failing to save an appointment", () => {
-  axios.put.mockRejectedValueOnce();
+//EDITS INTERVIEW
+it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+  const { container }  = render(<Application />);
+
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+  const appointment = getAllByTestId(container, "appointment").find(
+    appointment => queryByText(appointment, "Archie Cohen")
+  );
+  fireEvent.click(getByAltText(appointment, "Edit"));
+  fireEvent.change(getByPlaceholderText(appointment, "Enter Student Name"), {
+    target: { value: "Lydia Miller-Jones" }
+  });
+  fireEvent.click(getByText(appointment, "Save"));
+  expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+  await waitForElementToBeRemoved(() => getByText(appointment, "Saving"));
+  expect(getByText(appointment, "Lydia Miller-Jones")).toBeInTheDocument();
+  expect(getByAltText(appointment, "Edit")).toBeInTheDocument();
+  const day = getAllByTestId(container, "day").find(day => getByText (day, "Monday"));
+  expect(getByText(day, /1 spot remaining/i)).toBeInTheDocument();
 });
+
+
+
 
 });
